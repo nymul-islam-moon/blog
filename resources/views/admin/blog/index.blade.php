0911eb34945dcb9a2d0bb51bcc92374612cb07be
Nymul Islam Moon <towkir1997islam@gmail.com>
2023-10-20 17:39:44 +0600

    Update index.blade.php
    add dynamic title to blog index

--- resources/views/admin/blog/index.blade.php
+++ resources/views/admin/blog/index.blade.php
@@ -25,7 +25,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0">Blog List</h4>
+                        <h4 class="mb-sm-0">{{ $title }} List</h4>
 
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
